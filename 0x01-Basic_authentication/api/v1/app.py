ae4a161ab9c3421eb6ee340430883cfbfab291c7
nourister <nouristerjuma@gmail.com>
2024-05-31 13:02:02 +0000

    0x01-Basic_authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -16,7 +18,22 @@ auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
-if auth_type == BasicAuth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
@@ -24,20 +41,6 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
